b289704a27ab6b342fb99fbf54ce6d4987b2cf45
Илья Никишин <ilya.ruwork@gmail.com>
2026-07-05 00:43:58 +0200

    Modules

--- gustc/run-example.sh
+++ gustc/run-example.sh
@@ -1,5 +1,10 @@
+source=../examples/$1.gust
+if [ -d ../examples/$1 ]; then
+    source=../examples/$1/main.gust
+fi
+
 mkdir -p ../examples/build/$1
-cargo run ../examples/$1.gust --emit-c ../examples/build/$1/$1.c -o ../examples/build/$1/$1 && ../examples/build/$1/$1
+cargo run "$source" --emit-c ../examples/build/$1/$1.c -o ../examples/build/$1/$1 && ../examples/build/$1/$1
 
 if [ ! -s ../examples/build/$1/$1 ]; then
     rm -rf ../examples/build/$1
